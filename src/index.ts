915e0c0cd377053a923fec64b63209d3311d2258
thoratica <me@tica.fun>
2022-01-21 01:06:18 +0900

    remote-kakao-example

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { Server } from 'remote-kakao';
+import config from '../config.json';
+
+const server = new Server({ useKakaoLink: true });
+
+server.on('message', async (msg) => {
+  const prefix = '>';
+  if (!msg.content.startsWith(prefix)) return;
+
+  const args = msg.content.split(' ');
+  const cmd = args.shift()?.slice(prefix.length);
+
+  switch (cmd) {
+    case 'ping':
+      const timestamp = Date.now();
+      await msg.replyText('Pong!');
+      msg.replyText(`${Date.now() - timestamp}ms`);
+      break;
+    case 'kaling':
+      msg.replyKakaoLink({ id: 69201, args: { title: args[0], description: args[1] } });
+      break;
+  }
+});
+
+server.start(3000, config);
